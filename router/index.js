729882ceaee56465c7ddc946ea784c3a3ea5f26f
simonsg7 <ssernag7@gmail.com>
2024-11-05 04:31:19 -0500

    Add home route to main router

--- router/index.js
+++ router/index.js
@@ -1,8 +1,16 @@
+import express from 'express';
 import users from '../src/modules/users/user.routes.js';
 import tasks from '../src/modules/tasks/task.routes.js';
 import projects from '../src/modules/projects/project.routes.js';
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('home');
+});
+
 const routers = (app) => {
+    app.use('/', router);
     app.use('/users', users);
     app.use('/tasks', tasks);
     app.use('/projects', projects);
